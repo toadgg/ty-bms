f2c36e408b0d22ff9ee67ed942bd2c6640829f4f
zhangxl <toadgg@gmail.com>
2017-07-31 14:47:53 +0800

    修改首页欢迎语

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>天一PMS</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,9 +69,9 @@
     @if (Route::has('login'))
         <div class="top-right links">
             @if (Auth::check())
-                <a href="{{ url('/home') }}">Dashboard</a>
+                <a href="{{ url('/home') }}">控制面板</a>
             @else
-                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/login') }}">登录</a>
                 {{--<a href="{{ url('/register') }}">Register</a>--}}
             @endif
         </div>
